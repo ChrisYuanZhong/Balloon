ad75d017222979fc094a7396841f3f5826fee0e0
Zenz_- <zenzli714@outlook.com>
2023-04-11 16:16:03 -0600

    Revert "Merge branch 'main' of https://github.com/ChrisYuanZhong/Balloon"
    This reverts commit df582706331346a47fc06cb44fe0452b8e73de38, reversing
    changes made to 1804b23abadf2d6ad0f69a3dd3fe83d8edaeb68a.

--- DistanceSensor/DistanceSensor.ino
+++ DistanceSensor/DistanceSensor.ino
@@ -1,8 +1,6 @@
 // defines pins numbers
-const int trigPin = 2;
-const int echoPin = 4;
-//const int trigPin = 9;
-//const int echoPin = 10;
+const int trigPin = 9;
+const int echoPin = 10;
 // defines variables
 long duration;
 int distance;
